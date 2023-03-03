a061ba78df3343fcb84955bf18b174dc585edcc5
JelloMangune <enugnam.ollej@gmail.com>
2023-03-03 10:16:17 +0800

    first commit

---
+++ PDC03_MODULE2/Activity02.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity02 : ContentPage
+    {
+        public Activity02()
+        {
+            InitializeComponent();
+        }
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valuelabel.Text = args.NewValue.ToString("F3");
+        }
+    }
+}
---
+++ PDC03_MODULE2/Activity05.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace PDC03_MODULE2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Activity05 : ContentPage
+	{
+		ObservableCollection<employee> employees = new ObservableCollection<employee>();
+		public ObservableCollection<employee> Employees { get { return employees; } }
+
+		public Activity05 ()
+		{
+			InitializeComponent ();
+			{
+				lst.ItemsSource = employees;
+
+				employees.Add(new employee { DisplayName = "Jello Mangune", Position = "President", profileImage="profile1.png" });
+                employees.Add(new employee { DisplayName = "Calvin Pamandanan", Position = "Vice President", profileImage = "profile2.jpg" });
+                employees.Add(new employee { DisplayName = "Marcko Cruz", Position = "Secretary", profileImage = "https://media.tenor.com/_bTaLmoLSc4AAAAM/troll-pilled.gif" });
+
+            }
+		}
+	}
+}
